ce0134ff04f89eb2a57aaabc9d02fe4060aacbd9
nuix09 <136358529+nuix09@users.noreply.github.com>
2025-05-17 02:52:04 +0530

    command handler added | tested with ping ✅

--- src/index.js
+++ src/index.js
@@ -1,11 +1,61 @@
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
 const yunabot = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+yunabot.commands = new Collection();
+
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandsFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandsFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ("data" in command && "execute" in command) {
+      yunabot.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[!!WARNING!!] The command at ${filePath} is missing a property.`
+      );
+    }
+  }
+}
 
 yunabot.once(Events.ClientReady, (readyClient) => {
   console.log(`Logged in as ${readyClient.user.tag}`);
 });
 
+yunabot.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = yunabot.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+});
+
 yunabot.login(token);
-// console.log(token);
